55b7536e83e8975795a48e126606fffa67910fff
Alexander Bravo Martinez <abravo@pros.com>
2021-02-17 20:28:30 -0500

    Second chapter adding a simple front end using React
    adding cors using spring configuration

---
+++ multiplication/src/main/java/microservice/book/multiplication/configuration/WebConfiguration.java
@@ -0,0 +1,17 @@
+package microservice.book.multiplication.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Alexander Bravo
+ */
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+    }
+}
